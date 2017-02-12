e4196b4bb7878d9cad877658ecc50c00dea6f2a3
blackawa <3104kurokawa@gmail.com>
2017-02-12 11:02:13 +0900

    :sparkles: isuda logging

--- webapp/java/isuda/src/main/java/net/isucon6/qualify/service/SpamService.java
+++ webapp/java/isuda/src/main/java/net/isucon6/qualify/service/SpamService.java
@@ -2,8 +2,6 @@ package net.isucon6.qualify.service;
 
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,15 +13,12 @@ import org.springframework.web.client.RestTemplate;
 public class SpamService {
     @Autowired
     private RestTemplate isupamRestTemplate;
-    private Logger log = LoggerFactory.getLogger(SpamService.class);
 
     public boolean isSpam(String text) {
         MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>() {{
             add("content", text);
         }};
         ResponseEntity<Map> res = isupamRestTemplate.postForEntity("/", params, Map.class);
-        log.info("receipt response:" + res);
-        log.info("response body:" + res.getBody());
         return !Boolean.valueOf(String.valueOf(res.getBody().get("valid")));
     }
 }
