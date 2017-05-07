cc636e5235ebecf2e3135b21b53e0e553fc7ee79
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-05-08 01:01:55 +0900

    [Add] entry一覧のupdated_at columnにindex作成

---
+++ db/isuda_add_index.sql
@@ -0,0 +1,1 @@
+ALTER TABLE entry ADD INDEX updated_at_index(updated_at);
--- provisioning/image/db_update.sh
+++ provisioning/image/db_update.sh
@@ -6,7 +6,11 @@ cd $(dirname $0)
 myuser=root
 mypass=root
 
+isuda_mydb=isuda
 isutar_mydb=isutar
+
+# Isuda inxex作成
+mysql -u${myuser} -p${mypass} ${isuda_mydb} < ../../db/isuda_add_index.sql
 
 # Isutar index作成
 mysql -u${myuser} -p${mypass} ${isutar_mydb} < ../../db/isutar_add_index.sql
